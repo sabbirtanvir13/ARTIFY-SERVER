b3402f625f514d6662c68bbcd5cdf580537ef575
Tanvir <sabbirtanvirahmed18@gmail.com>
2025-11-12 20:14:54 +0600

    added latest tiem

--- index.js
+++ index.js
@@ -1,100 +1,7 @@
-// const express = require('express');
-// const cors = require('cors');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const app = express()
-// const port =process.env.PORT || 3000
-
-// app.use(cors());
-// app.use(express.json())
-
-// const uri = "mongodb+srv://artify_db:W52DpwrCqs9k0UHO@cluster0.tx061fa.mongodb.net/?appName=Cluster0";
-
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// app.get('/', (req, res) => {
-//   res.send('Artify server is runnig')
-// })
-
-// async function run() {
-//   try {
-
-//     await client.connect();
-
-//     const db =client.db('artify_db')
-//     const artifyCollection=db.collection('artifys')
-
-//   //  app.get('/artifys',async(req,res)=>{
-//   //   const result=await artifyCollection.find().sort({ createdAt: -1 }).limit(6).toArray();
-     
-//   //   res.send(result)
-//   //  })
-// //   app.get('/featured-artworks', async (req, res) => {
-// //   const result = await artifyCollection.find().sort({ createdAt: -1 }).limit(6).toArray();
-// //   res.send(result);
-// // });
-
-//    app.get('/artifys',async(req,res)=>{
-//     const result =await artifyCollection.find().toArray();
-//       res.send(result)
-//    })
-
-//   //  app.post('/artifys',async(req,res)=>{
-//   //  const data= req.body
-//   //  console.log(data)
-//   //   const result =await artifyCollection.insertOne(data)
-//   //   res.send({
-//   //     sussces:true,
-//   //     result
-//   //   })
-//   //   //  res.send(result);
-//   //  })
-
-//   app.post('/artifys', async (req, res) => {
-//   try {
-//     const data = req.body;
-//     console.log('Received data:', data);
-
-//     const result = await artifyCollection.insertOne(data);
-//     console.log('Inserted Result:', result);
-
-//     if(result.insertedId){
-//       res.send({ success: true, insertedId: result.insertedId });
-//     } else {
-//       res.status(500).send({ success: false, message: "Insert failed" });
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send({ success: false, message: err.message });
-//   }
-// });
-
-
-
-   
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-
-  
-//   }
-// }
-// run().catch(console.dir);
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
-
 
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -103,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB URI
+
 const uri = "mongodb+srv://artify_db:W52DpwrCqs9k0UHO@cluster0.tx061fa.mongodb.net/artify_db?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
@@ -121,12 +28,19 @@ async function run() {
 
     const db = client.db('artify_db');
     const artifyCollection = db.collection('artifys');
-
+     
     // GET all artworks
     app.get('/artifys', async (req, res) => {
       const result = await artifyCollection.find().toArray();
       res.send(result);
     });
+
+  app.get('/artifys/:id', async (req, res) => {
+  const id = req.params.id;
+  const result = await artifyCollection.findOne({ _id: new ObjectId(id) });
+  res.send({ success: true, result });
+});
+
 
     
 app.post('/artifys', async (req, res) => {
@@ -134,6 +48,69 @@ app.post('/artifys', async (req, res) => {
   const result = await artifyCollection.insertOne(data);
   res.send({ insertedId: result.insertedId });
 });
+
+
+ app.get('/latest-artifys', async (req, res) => {
+  const result = await artifyCollection
+    .find()
+    .sort({ created_at: -1 }) 
+    .limit(6)
+    .toArray();
+
+  res.send(result);
+  console.log(result);
+});
+
+
+    
+
+    app.post('/artifys', async (req, res) => {
+  const data = req.body;
+  data.createdAt = new Date();
+  const result = await artifyCollection.insertOne(data);
+  const newArtwork = await artifyCollection.findOne({ _id: result.insertedId });
+  res.send({ success: true, artwork: newArtwork });
+});
+
+
+
+app.get('/explore-artworks', async (req, res) => {
+  const result = await artifyCollection
+    .find({ visibility: "Public" })
+    .toArray();
+
+  res.send(result);
+});
+
+
+
+// ✅ Get artworks by user email
+app.get("/my-artworks", async (req, res) => {
+  const email = req.query.email;
+  const result = await artifyCollection.find({ created_at: email }).toArray()
+  res.send(result);
+});
+
+// ✅ Delete artwork
+app.delete("/artifys/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await artifyCollection.deleteOne({ _id: new ObjectId(id) });
+  res.send(result);
+});
+
+// ✅ Update artwork
+app.put("/artifys/:id", async (req, res) => {
+  const id = req.params.id;
+  const updated = req.body;
+  const result = await artifyCollection.updateOne(
+    { _id: new ObjectId(id) },
+    { $set: updated }
+  );
+  res.send(result);
+});
+
+
+
 
 
     app.get('/', (req,res) => {
